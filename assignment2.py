ce9cf722e59c202ffff2b0b5c0aeef5ffecf6bca
Damon <eccdc001@mymail.unisa.edu.au>
2023-10-22 11:57:19 +1030

    Testing: Check if clone worked

---
+++ assignment2.py
@@ -0,0 +1,9 @@
+'''
+File: .py
+Description: 
+Author: Damon Eccles
+StudentID: 110401198
+EmailID: eccdc001@mymail.edu.au
+This is my own work as defined by the University's Academic Misconduct Policy.
+'''
+
